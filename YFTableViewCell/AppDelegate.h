74ae6cdfe87c45c89d6785b0240b451639bf7c0c
saxue <mkshow@126.com>
2015-11-03 21:48:19 +0800

    Initial Commit

---
+++ YFTableViewCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YFTableViewCell
+//
+//  Created by 杨枫 on 15/11/3.
+//  Copyright © 2015年 Saxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YFTableViewCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YFTableViewCell
+//
+//  Created by 杨枫 on 15/11/3.
+//  Copyright © 2015年 Saxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
